27d40e3d365a70de59af89bb519b5b5532252825
CleetSR388 <41758594+CleetSR388@users.noreply.github.com>
2018-07-27 06:38:36 -0400

    removed ssp files

---
+++ Assets/Firing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+
+    [SerializeField] AudioClip impact;
+    [SerializeField] AudioSource audioSource;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        audioSource.PlayOneShot(impact, .7f);
+    }
+
+}
